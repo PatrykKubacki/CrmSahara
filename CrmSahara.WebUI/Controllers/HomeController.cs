62f9aab3c15f8ef7d2e5979db29276e909062a3b
Patryk Kubacki <patryk.kubacki@outlook.com>
2017-08-30 14:39:57 +0200

    Init Project

---
+++ CrmSahara.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using CrmSahara.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrmSahara.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
